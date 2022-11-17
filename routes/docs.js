8d9d6bf4260fdd4a60ccff14588b9fcdcec697e4
dwip <cumadwip@gmail.com>
2022-11-17 21:19:44 +0700

    work in progress

--- routes/docs.js
+++ routes/docs.js
@@ -4,11 +4,17 @@ const router = express.Router()
 const swaggerUi = require('swagger-ui-express')
 const apiDoc2022021621 = require('../documentations/apiDoc2022021621.json')
 const apiDoc2022052021 = require('../documentations/apiDoc2022052021.json')
+const apiDoc2022111715 = require('../documentations/apiDoc2022111715.json')
 const apiDocNakes = require('../documentations/apiDocNakes.json')
 const apiDocRSOnlineRS = require('../documentations/apiDocRSOnlineRS.json')
 
 router.use('/apidoc2022021621', swaggerUi.serve, (req, res) => {
     let html = swaggerUi.generateHTML(apiDoc2022021621);
+    res.send(html);
+})
+
+router.use('/apiDoc2022111715', swaggerUi.serve, (req, res) => {
+    let html = swaggerUi.generateHTML(apiDoc2022111715);
     res.send(html);
 })
 
