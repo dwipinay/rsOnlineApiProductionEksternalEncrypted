1042c53a204dfe95a069a0ba450fa960bb256cf7
dwip <cumadwip@gmail.com>
2023-08-03 13:24:17 +0700

    work in progress

--- routes/docs.js
+++ routes/docs.js
@@ -8,10 +8,16 @@ const apiDoc2022111715 = require('../documentations/apiDoc2022111715.json')
 const apiDocNakes = require('../documentations/apiDocNakes.json')
 const apiDocRSOnlineRS = require('../documentations/apiDocRSOnlineRS.json')
 const apiDoc2022122910 = require('../documentations/apiDoc2022122910.json')
+const apiDocBapeten1 = require('../documentations/apiDocsBapeten-1.json')
 const apiDocDinkesSurakarta1 = require('../documentations/apiDocsDinkesSurakarta-1.json')
 
 router.use('/apidoc2022021621', swaggerUi.serve, (req, res) => {
     let html = swaggerUi.generateHTML(apiDoc2022021621);
+    res.send(html);
+})
+
+router.use('/apiDocBapeten-1', swaggerUi.serve, (req, res) => {
+    let html = swaggerUi.generateHTML(apiDocBapeten1);
     res.send(html);
 })
 
